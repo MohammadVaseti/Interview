1d2b05172ba9230d5fc70802c1819f81443f3f0b
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-06-01 13:49:16 +0330

    repo updated

---
+++ Challenges/script.js
@@ -0,0 +1,22 @@
+// reverse a string
+function reverse(str) {
+  let reversed = "";
+  for (let a = str.length - 1; a >= 0; a--) {
+    reversed += str[a];
+  }
+  console.log(reversed);
+}
+reverse("hana");
+/////////////////////////////////////
+
+// find the biggest number in a array
+const arr = [1, 4, 2, 56, 87, 98, 198, 75];
+let num = arr[0];
+for (let a = 0; a <= arr.length - 1; a++) {
+  if (num < arr[a]) {
+    num = arr[a];
+  }
+}
+console.log(num);
+
+// palindrome
